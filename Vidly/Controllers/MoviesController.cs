cb15e0c8372b1928614a75fcc9ed9530eac9a0a0
zubair-git <zubairhussain76137@gmail.com>
2017-05-04 16:18:39 -0500

    5/4/2017

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+
+            return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            
+        }
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id=" + Id); 
+        }
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+              
+        }
+
+
+    }
+
+}
+
+
+
